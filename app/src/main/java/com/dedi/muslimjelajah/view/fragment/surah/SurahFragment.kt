324af1f66de73348eab242db890aaf04aca11d63
deditian <deditian.n.h@gmail.com>
2021-10-05 11:03:15 +0700

    - add handle network in SurahFragment - add hiltDagger di

--- app/src/main/java/com/dedi/muslimjelajah/view/fragment/surah/SurahFragment.kt
+++ app/src/main/java/com/dedi/muslimjelajah/view/fragment/surah/SurahFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dedi.muslimjelajah.R
 import com.dedi.muslimjelajah.databinding.FragmentSurahBinding
+import com.dedi.muslimjelajah.domain.ResultState
+import com.dedi.muslimjelajah.domain.entity.Surah
+import com.dedi.muslimjelajah.utils.onFailure
 import com.dedi.muslimjelajah.utils.onSuccess
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,6 +37,8 @@ class SurahFragment : Fragment(R.layout.fragment_surah) {
 
         viewModel.surah.observe(viewLifecycleOwner){ result ->
 
+            networkUI(result)
+
             result.onSuccess { surah ->
                 Log.e("TAG", "onView: asd surah $result" )
                 surahAdapter.setList(surah)
@@ -40,6 +46,22 @@ class SurahFragment : Fragment(R.layout.fragment_surah) {
 
         }
 
+    }
+
+    private fun FragmentSurahBinding.networkUI(result: ResultState<List<Surah>>) = layoutNetwork.run {
+        val isLoading = result is ResultState.Loading
+        val isError = result is ResultState.Error
+        btnTryAgain.setOnClickListener {
+            viewModel.getSurah()
+        }
+
+        prNetwork.isVisible = isLoading
+        btnTryAgain.isVisible = isError
+        tvError.isVisible = isError
+
+        result.onFailure { th ->
+            tvError.text = th.message
+        }
     }
 
 
